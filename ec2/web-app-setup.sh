77160ce28e3a1e8f594cf94507f1ff5d358f3eba
Ankit Singh <ankitkrsingh13@gmail.com>
2024-05-03 12:31:51 +0530

    Added User-data script to auto-mount secondary volume

--- ec2/web-app-setup.sh
+++ ec2/web-app-setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install -y httpd
+sudo yum install -y httpd xfsprogs
 sudo systemctl start httpd
 sudo systemctl enable httpd
 echo "Hello World from $(hostname)" > /var/www/html/index.html
-#ToDo
-# Configure logging for httpd
-# Pull Web app from Github Repo
+
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+DEVICE=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/block-device-mapping/ebs2)
+BLOCK=$(ls -l /dev/${DEVICE} | awk '{print $NF}')
+sudo mkfs -t xfs /dev/${BLOCK}
+sudo mkdir -p /var/log
+sudo mount /dev/${BLOCK} /var/log
+
+UUID=$(sudo blkid -s UUID -o value /dev/${BLOCK})
+echo "UUID=${UUID}  /var/log  xfs  defaults,nofail  0  2" | sudo tee -a /etc/fstab
